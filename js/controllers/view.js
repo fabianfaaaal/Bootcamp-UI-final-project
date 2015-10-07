df66c069a8d65c367b686a3b60c109e8f8221cf3
fabianfaaaal <fabian.lammoglia@gmail.com>
2015-10-07 16:29:39 -0300

    Updated files

--- js/controllers/view.js
+++ js/controllers/view.js
@@ -1,33 +1,34 @@
 app.controller('viewCtrl', ['$scope','twitterdata', function ($scope, Factory) {
 
 	console.log("viewCtrl working!"); 
-	$scope.title = "Nombre Twitter";
-	
+	$scope.title = "Tiemeline";
+	$scope.tweets = [] ;
+
 	var callback = function(data) {
 		console.log("ajax done");
 		//console.log(data);
 
-		var tweets = [] ;
-
 		tweets = data;
 		
 
-		//if(tweets.length) {
+		if(tweets.data.length) {
 
-			for (var i = 0; i < tweets.length; i++) {
+			for (var i = 0; i < tweets.data.length; i++) {
 
 				console.log("Nombre nº " + i + " = " + tweets.data[i].user.screen_name);
 			};
 
 			
-		//};
+		};
 		
 		console.log(tweets);
-		
+
 		return tweets;
 	};
 
 	Factory.get(callback);
+
+
 
 	
 
